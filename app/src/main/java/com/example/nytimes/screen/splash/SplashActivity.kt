439f7c75c641147ff5b1cd8d57f13584063e86f3
ijazahmad131 <37957582+ijazahmad131@users.noreply.github.com>
2021-10-09 22:50:06 +0530

    test app

--- app/src/main/java/com/example/nytimes/screen/splash/SplashActivity.kt
+++ app/src/main/java/com/example/nytimes/screen/splash/SplashActivity.kt
@@ -1,12 +1,16 @@
 package com.example.nytimes.screen.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nytimes.R
+import com.example.nytimes.screen.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val i: Intent = Intent(this,HomeActivity::class.java)
+        startActivity(i)
     }
 }
